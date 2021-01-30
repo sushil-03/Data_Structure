98848a817c1fe40c67ea4709f338b97efea963b8
sushil-03 <rawatjojo500@gmail.com>
2021-01-30 22:46:38 +0530

    30

---
+++ String/89.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str="geeksforgeeks";
+    int n=str.length();
+    unordered_map<int,int>m;
+    int max=0;
+    for(int i=0;i<n;i++){
+        m[str[i]]++;
+
+        if(m[str[i]]>max){
+            max=m[str[i]];
+        }
+    }
+    if(max<=n-max+1) cout<<"1"<<endl;
+    else
+    cout<<"0"<<endl;
+
+    return 0;
+}
