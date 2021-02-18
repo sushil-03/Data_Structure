a9c6ce7ff2b0d9812a566763353eafa1272d71dc
sushil <rawatjojo500@gmail.com>
2021-02-18 14:45:39 +0530

    queen

---
+++ Recursion_&_Backtracking/3.cpp
@@ -0,0 +1,36 @@
+//N Queen Problem using Backtracking(Branch and Bound)
+//  place queen in position so that no queen intersect each other
+#include<bits/stdc++.h>
+#define n 8
+using namespace std;
+
+void Solve(bool board[][n] , int row,bool cols[],bool ndiag[],bool rdiag[],string anf){
+    
+    if(row==n){
+        cout<<anf<<".";
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(cols[i]==false && ndiag[row+i]==false && rdiag[row-i+n-1]==false){
+            board[row][i]=true;
+             cols[i]=true;
+            ndiag[row+i]=true;
+            rdiag[row-i+n-1]=true;
+            Solve(board,row+1,cols,ndiag,rdiag,anf+to_string(row)+"-"+to_string(i)+",  ");
+            board[row][i]=false;
+             cols[i]=false;
+            ndiag[row+i]=false;
+            rdiag[row-i+n-1]=false;
+        }
+    }
+}
+int main(){
+   
+    bool board[n][n]={0};
+    
+    bool cols[n]={0};
+    bool ndiag[2  * n - 1]={0};//new diagnol
+    bool rdiag[2  * n - 1]={0};//reverse diagnol
+    Solve(board,0,cols,ndiag,rdiag,"");
+    return 0;
+}
