c163365b1a59ee15b14a7bc7a787a6c8367a62c4
sushil <rawatjojo500@gmail.com>
2021-02-18 17:28:38 +0530

    0

--- Recursion_&_Backtracking/3.cpp
+++ Recursion_&_Backtracking/3.cpp
@@ -1,4 +1,4 @@
-//N Queen Problem using Backtracking(Branch and Bound)
+/s//N Queen Problem using Backtracking(Branch and Bound)
 //  place queen in position so that no queen intersect each other
 #include<bits/stdc++.h>
 #define n 8
@@ -7,13 +7,13 @@ using namespace std;
 void Solve(bool board[][n] , int row,bool cols[],bool ndiag[],bool rdiag[],string anf){
     
     if(row==n){
-        cout<<anf<<".";
+        cout<<anf<<"."  `1;
         return;
     }
     for(int i=0;i<n;i++){
         if(cols[i]==false && ndiag[row+i]==false && rdiag[row-i+n-1]==false){
             board[row][i]=true;
-             cols[i]=true;
+            cols[i]=true;
             ndiag[row+i]=true;
             rdiag[row-i+n-1]=true;
             Solve(board,row+1,cols,ndiag,rdiag,anf+to_string(row)+"-"+to_string(i)+",  ");
